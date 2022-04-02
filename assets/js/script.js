cb69c14d853dbf61a3b17874f595b7f91fbd7b06
mcelhatton <colinm@mobiledealerdata.com>
2022-04-02 10:32:07 -0500

    saving working code loading data to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 const apiKey = 'c3cbe84d45eaa63cacdc46b1a5933fb1'
 
+var city = '';
+var citySearchEl = $('#citySearch');
+var tempEl = $('#temp');
+var windEl = $('#wind');
+var humidityEl = $('#humidity');
+var uvEl = $('#uv');
+
 var cityFormEl = document.querySelector('#cityForm');
 var cityInputEl = document.querySelector('#city');
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
-  var city = cityInputEl.value.trim();
-  console.log(city);
+  city = cityInputEl.value.trim();
 
   if (city) {
     getCityLatLong(city);
@@ -35,14 +41,31 @@ var getCityLatLong = function(city) {
 }
 
 var getWeatherData = function(lat, long) {
-  var getWeatherURL = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=5&appid=${apiKey}`;
+  var getWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}`;
 
   fetch(getWeatherURL).then(function(response) {
     response.json().then(function(data) {
       console.log(data);
+      loadWeatherData(data);
     });
   });
   console.log(getWeatherURL);
+}
+
+function loadWeatherData(data) {
+  citySearchEl.textContent = '';
+  temp.textContent = '';
+  windEl.textContent = '';
+  humidityEl.textContent = '';
+  uvEl.textContent = '';
+
+  citySearch.textContent = city;
+  temp.textContent = data.current.temp;
+  wind.textContent = data.current.wind_speed;
+  humidity.textContent = data.current.humidity;
+  uv.textContent = data.current.uvi;
+  
+
 }
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
