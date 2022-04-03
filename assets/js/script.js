0991b795b2eb282aac2da3e90d64ffcd2bc9b960
mcelhatton <colinm@mobiledealerdata.com>
2022-04-03 17:47:11 -0500

    saving updated code with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+// openweathermap.org/ apiKey -  see site for api documentation
 const apiKey = 'c3cbe84d45eaa63cacdc46b1a5933fb1';
 
+// loads current date into html
 var timeNow = moment().format('MMMM Do YYYY');
 $('#date').replaceWith(timeNow);
 
+// HTML variables
 var city = '';
 var tempEl = $('#temp');
 var windEl = $('#wind');
@@ -17,13 +20,13 @@ var citySearchDisplayEl = document.querySelector('#citySearch');
 $('#curentWeatherDiv').hide();
 $('#5dayForecastDiv').hide();
 
+// sumit button handler code
 var formSubmitHandler = function(event) {
   event.preventDefault();
   city = cityInputEl.value.trim();
   $('#curentWeatherDiv').show();
   $('#5dayForecastDiv').show();
   
-
   if (city) {
     getCityLatLong(city);
     //$('#citySearch').replaceWith(city);
@@ -73,7 +76,6 @@ function loadWeatherData(data) {
   var currentWeatherIconUrl = `http://openweathermap.org/img/wn/${weatherIconID}@2x.png`;
   var uvIndex = data.current.uvi;
 
-  
   temp.textContent = '';
   windEl.textContent = '';
   humidityEl.textContent = '';
@@ -87,6 +89,7 @@ function loadWeatherData(data) {
   humidity.textContent = data.current.humidity;
   uv.textContent = data.current.uvi;
 
+  // color codes the UV index
   if (uvIndex < 3) {
     $('#uv').css({'background-color': '#6DD400', 'color': 'black'});
   } else if (uvIndex < 6) {
@@ -100,7 +103,6 @@ function loadWeatherData(data) {
   // clears out weather data
   for (var i = 1; i < 6; i++) {
     $(`#list${i}`).children('li').remove();
-
   }
 
   // load data into cards 5 day forecast
@@ -119,7 +121,7 @@ function loadWeatherData(data) {
     $(`#list${i}`).append(`<li>High Temp: ${temp5day}</li>`);
     $(`#list${i}`).append(`<li>Wind Speed: ${wind5day}</li>`);
     $(`#list${i}`).append(`<li>Humidity: ${humidity5day}</li>`);
-    $(`#list${i}`).append(`<li>UV Index: ${uv5day}</li>`);
+    $(`#list${i}`).addClass("badge badge-primary badge-pill").append(`<li>UV Index: ${uv5day}</li>`);
     
   }
  
@@ -127,10 +129,11 @@ function loadWeatherData(data) {
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
 
-
+// loads historical search items 
 function getID(btn) {
   var pastCity = btn.id;
   city = pastCity;
   citySearchDisplayEl.textContent = pastCity;
   getCityLatLong(city);
 }
+
